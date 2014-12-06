e6e469a32642f0b09337a5dac61f7e951522bad9
mokumoku <da1_1hara@me.com>
2014-12-06 20:06:21 +0900

    Initial Commit

---
+++ FabricTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FabricTest
+//
+//  Created by Daichi Ichihara on 2014/12/06.
+//  Copyright (c) 2014年 MokuMokuCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
